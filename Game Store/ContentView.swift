a4b927f43e3bb87e2d2fe81e8735a579d49cd9cb
Murat Gültürk <m.gueltuerk@gmx.de>
2022-05-09 16:37:21 +0200

    Initial Commit

---
+++ Game Store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game Store
+//
+//  Created by Murat Gültürk on 09.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game Store/Game_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_StoreApp.swift
+//  Game Store
+//
+//  Created by Murat Gültürk on 09.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Game_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
